d8577b1d14473f94842eda6c4838ebe1c42b29db
Sam <sam.a.menza@gmail.com>
2015-04-06 03:50:13 -0400

    Commited change in submodule gazette

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'commonplace');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
